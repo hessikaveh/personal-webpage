9dc2f9e5bae125b4f10b226744fb20143d57738e
hessikaveh <hesam.kaveh@gmail.com>
2023-07-29 01:51:21 +0200

    first draft of a personal homepage

---
+++ app/projects/page.tsx
@@ -0,0 +1,30 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <article className="prose">
+        <h1>Sustainable supply chains</h1>
+        <p>
+          For years parents have espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </p>
+        <h1>Hunting for Higgs</h1>
+        <p>
+          For years parents have espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </p>
+
+        <h1>Building virtual skies</h1>
+        <p>
+          For years parents have espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </p>
+      </article>
+    </main>
+  );
+}

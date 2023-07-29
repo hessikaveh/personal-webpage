8ddf846b3208fb0b47fe78ffe0051eb7b4ab1814
hessikaveh <hesam.kaveh@gmail.com>
2023-07-29 13:57:05 +0200

    adding some details

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,29 +1,23 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <article className="prose">
         <h1>Sustainable supply chains</h1>
-        <p>
-          For years parents have espoused the health benefits of eating garlic
-          bread with cheese to their children, with the food earning such an
-          iconic status in our culture that kids will often dress up as warm,
-          cheesy loaf for Halloween.
-        </p>
+        <p>kdfdj.</p>
         <h1>Hunting for Higgs</h1>
-        <p>
-          For years parents have espoused the health benefits of eating garlic
-          bread with cheese to their children, with the food earning such an
-          iconic status in our culture that kids will often dress up as warm,
-          cheesy loaf for Halloween.
-        </p>
-
+        <p>Higgs</p>
+        <div>
+          <Image
+            src="/higgs_frame.svg"
+            alt="higgs analysis overview"
+            width={900}
+            height={450}
+          ></Image>
+        </div>
         <h1>Building virtual skies</h1>
-        <p>
-          For years parents have espoused the health benefits of eating garlic
-          bread with cheese to their children, with the food earning such an
-          iconic status in our culture that kids will often dress up as warm,
-          cheesy loaf for Halloween.
-        </p>
+        <p>dome</p>
       </article>
     </main>
   );

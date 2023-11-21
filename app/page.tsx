0a4bb378623c49e0df6e1e855f2c0f62a3498e67
hessikaveh <hesam.kaveh@gmail.com>
2023-11-21 19:13:46 +0100

    add google analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 import Link from "next/link";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <main className="flex min-h-fit flex-col items-center justify-between p-24">
+      <div className="container">
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-EV51J1SKL9" />
+        <Script id="google-analytics">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-EV51J1SKL9');
+      `}
+        </Script>
+      </div>
       <article className="prose lg:prose-xl">
         <h1>Sam Kaveh, PhD</h1>
         <h2>About Me</h2>

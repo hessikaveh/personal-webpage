8346af68d4b7f496b6a361b716016284d6bf84f5
hessikaveh <hesam.kaveh@gmail.com>
2026-02-14 13:14:14 +0000

    Deploying to gh-pages from @ hessikaveh/personal-webpage@dc35fdd0f2f814920cd185aca97d5e22d53e2fb6 🚀

---
+++ _next/static/V85fEy4Tijv9O-lqWPXji/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/personal-webpage//_next/:path+",
+        "destination": "/personal-webpage/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
